0c88b77857d1a1c01902a989e2c783b30dda5c10
ChWick <ch.wick@web.de>
2013-12-12 11:29:38 +0100

    Added notes for the modifications of the ogre-paged files

--- dependencies/ogre-paged/source/BatchedGeometry.cpp
+++ dependencies/ogre-paged/source/BatchedGeometry.cpp
@@ -1,4 +1,10 @@
 /*-------------------------------------------------------------------------------------
+Modified by Christoph Wick (ch.wick@web.de)
+Wrong orientation of mesh normals
+https://code.google.com/p/ogre-paged/issues/detail?id=11
+-------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------
 Copyright (c) 2006 John Judnich
 
 This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
