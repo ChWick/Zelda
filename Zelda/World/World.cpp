9e76cc7c08bb37c4969b8a47d96e7735be5c5b8a
C. Wick <ch.wick@web.de>
2014-08-12 14:34:48 +0200

    added position detection of camera bounds on plane

--- Zelda/World/World.cpp
+++ Zelda/World/World.cpp
@@ -7,6 +7,11 @@
 #include <OgreCamera.h>
 #include <OgreViewport.h>
 #include "Camera/AerialCameraPerspective.hpp"
+#include "../Common/Log.hpp"
+#include "../Common/Util/DebugDrawer.hpp"
+
+// drow camera bounds (lines and spheres)
+#define WORLD_DEBUG_CAMERA_BOUNDS
 
 CWorld::CWorld()
   : CGameState(GST_WORLD),
@@ -37,6 +42,8 @@ CWorld::CWorld()
 
   //m_pCameraPerspective = new CAerialCameraPerspective(m_pWorldCamera, (Ogre::SceneNode*)m_pAtlas->getChildren().front()->getSceneNode()->getChild(0));
   m_pCameraPerspective = new CAerialCameraPerspective(m_pWorldCamera, m_pPlayer->getSceneNode());
+
+  setupEdgeRays();
 }
 
 CWorld::~CWorld() {
@@ -48,9 +55,40 @@ CWorld::~CWorld() {
 void CWorld::update(Ogre::Real tpf) {
   CGameState::update(tpf);
   m_pCameraPerspective->updateCamera(tpf);
+
+#ifdef WORLD_DEBUG_CAMERA_BOUNDS
+  for (int i = 0; i < RAY_COUNT; i++) {
+    Ogre::Ray r = m_pWorldCamera->getCameraToViewportRay(i % 2, i / 2);
+    DebugDrawer::getSingleton().drawLine(r.getOrigin(), r.getPoint(1000), Ogre::ColourValue::Red);
+
+    DebugDrawer::getSingleton().drawSphere(getRayPlaneHitPosition(i), 0.2, Ogre::ColourValue::Red, false);
+  }
+#endif
 }
 
 bool CWorld::frameRenderingQueued(const Ogre::FrameEvent& evt) {
 
   return true;
+}
+
+void CWorld::setupEdgeRays() {
+  const Ogre::Radian beta(m_pWorldCamera->getFOVy());
+  const Ogre::Radian alpha(Ogre::Math::ATan(Ogre::Math::Tan(beta) * m_pWorldCamera->getAspectRatio()));
+
+  LOGV("View angles x='%f' y='%f'", alpha.valueDegrees(), beta.valueDegrees());
+
+  const Ogre::Vector3 vLeftRotXAxis(Ogre::Quaternion(alpha, Ogre::Vector3::UNIT_Y).xAxis());
+  const Ogre::Vector3 vRightRotXAxis(-(Ogre::Quaternion(-alpha, Ogre::Vector3::UNIT_Y).xAxis()));
+
+  m_vEdgeRayDirections[RAY_TOP_LEFT] = -(Ogre::Quaternion(-beta, vLeftRotXAxis) * Ogre::Quaternion(alpha, Ogre::Vector3::UNIT_Y).zAxis());
+  m_vEdgeRayDirections[RAY_BOTTOM_LEFT] = -(Ogre::Quaternion(beta, vLeftRotXAxis) * Ogre::Quaternion(alpha, Ogre::Vector3::UNIT_Y).zAxis());
+  m_vEdgeRayDirections[RAY_TOP_RIGHT] = -(Ogre::Quaternion(-beta, vRightRotXAxis) * Ogre::Quaternion(-alpha, Ogre::Vector3::UNIT_Y).zAxis());
+  m_vEdgeRayDirections[RAY_BOTTOM_RIGHT] = -(Ogre::Quaternion(beta, vRightRotXAxis) * Ogre::Quaternion(-alpha, Ogre::Vector3::UNIT_Y).zAxis());
+}
+
+Ogre::Vector3 CWorld::getRayPlaneHitPosition(int iRayIndex, Ogre::Real fPlaneY) const {
+  Ogre::Ray r = m_pWorldCamera->getCameraToViewportRay(iRayIndex % 2, iRayIndex / 2);
+  std::pair<bool, Ogre::Real> bIntersection = r.intersects(Ogre::Plane(Ogre::Vector3::UNIT_Y, fPlaneY));
+
+  return r.getPoint(bIntersection.second);
 }
--- Zelda/World/World.hpp
+++ Zelda/World/World.hpp
@@ -14,12 +14,27 @@ private:
   CAtlas *m_pAtlas;
   Ogre::Camera *m_pWorldCamera;
   CAerialCameraPerspective *m_pCameraPerspective;
+
+  enum ECameraEdgeRays {
+    RAY_TOP_LEFT = 0,
+    RAY_TOP_RIGHT,
+    RAY_BOTTOM_LEFT,
+    RAY_BOTTOM_RIGHT,
+    RAY_COUNT
+  };
+
+  Ogre::Vector3 m_vEdgeRayDirections[RAY_COUNT];
+
 public:
   CWorld();
   ~CWorld();
 
   void update(Ogre::Real tpf);
   bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
+private:
+  void setupEdgeRays();
+  Ogre::Vector3 getRayPlaneHitPosition(int iRayIndex, Ogre::Real fPlaneY = 0) const;
 };
 
 #endif // _WORLD_HPP_
