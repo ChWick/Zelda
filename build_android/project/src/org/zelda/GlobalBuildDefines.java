991d203adeccde498e411200b05a73c7b9766dfc
C. Wick <ch.wick@web.de>
2014-08-19 16:41:15 +0200

    added build android files

---
+++ build_android/project/src/org/zelda/GlobalBuildDefines.java
@@ -0,0 +1,45 @@
+package org.zelda;
+
+public final class GlobalBuildDefines {
+    // define version
+    public final static int PROJECT_VERSION_CODE = 1;
+    public final static String PROJECT_VERSION_NAME = "0.0.1";
+
+    // Flag to define whether to include the map editor
+    public final static boolean ENABLE_MAP_EDITOR = 0 == 1;
+    
+    // Flag to define whether to enable the campaign
+    public final static boolean ENBLE_CAMPAIGN = 0 == 1;
+
+    // Flag to define whether to enable cheats
+    public final static boolean ENABLE_CHEATS = 0 == 1;
+
+    // Flag to define wheter to enable touch-screen input
+    public final static boolean ENABLE_INPUT_TOUCH = 0 == 1;
+
+    // Index of the input platform (0: keyboard, 1: touch, 2: amazon remote control)
+    public final static int PROJECT_INPUT_PLATFORM = 0;
+
+    // define whether there are input buttons
+    public final static boolean PROJECT_HAS_INPUT_BUTTONS_IMAGESET = PROJECT_INPUT_PLATFORM == 0 || PROJECT_INPUT_PLATFORM == 2;
+
+    // Flag to enable debug output
+    public final static boolean ENABLE_DEBUG_DEFINES = 0 == 1;
+
+    // Enable Shader manager (required for GLESES, optional for OpenGL/DirectX)
+    public final static boolean PROJECT_ENABLE_SHADER = 1 == 1;
+
+    // Assert mode
+    // 0: not asserting (release)
+    // 1: asserts (debug)
+    public final static int PROJECT_ASSERT_MODE = 1;
+
+    // Ads enabled
+    public final static boolean PROJECT_ENABLE_ADS = 0 == 1;
+
+    // use amazon game circle
+    public final static boolean PROJECT_USE_AMAZON_GAME_CIRCLE = 0 == 1;
+
+    // default login to social gaming
+    public final static boolean PROJECT_DEFAULT_LOGIN_TO_SOCIAL_GAMING = 0 == 1;
+}
---
+++ build_android/project/src/org/zelda/Plugin.java
@@ -0,0 +1,72 @@
+/*****************************************************************************
+ * Copyright 2014 Christoph Wick
+ *
+ * This file is part of Mencus.
+ *
+ * Mencus is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Mencus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Mencus. If not, see http://www.gnu.org/licenses/.
+ *****************************************************************************/
+
+package org.zelda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class Plugin {
+	protected Activity mActivity;
+	private boolean mStarted;
+	
+	public Plugin(Activity activity) {
+		mActivity = activity;
+		mStarted = false;
+	}
+	
+	public boolean isStarted() {return mStarted;}
+	public void start() {
+		mStarted = true;
+		onCreate();
+		onResume();
+	}
+	
+	public void onCreate() {
+		if (mStarted) {
+			onCreateImpl();
+		}
+	}
+	public void onDestroy() {
+		if (mStarted) {
+			onDestroyImpl();
+		}
+	}
+	public void onPause() {
+		if (mStarted) {
+			onPauseImpl();
+		}
+	}
+	public void onResume() {
+		if (mStarted) {
+			onResumeImpl();
+		}
+	}
+	public void onStop() {
+		if (mStarted) {
+			onStopImpl();
+		}
+	}
+	
+	protected abstract void onCreateImpl();
+	protected abstract void onDestroyImpl();
+	protected abstract void onPauseImpl();
+	protected abstract void onResumeImpl();
+	protected abstract void onStopImpl();
+}
